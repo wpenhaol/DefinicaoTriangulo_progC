1f817842d42fb85b4e3fc98ea7bea906a1bf758a
William Penha <penhadr12@users.noreply.github.com>
2016-03-28 15:07:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,51 @@
 
 int main()
 {
-    // declaracao de variaveis
+	// declaracao das variaveis
     float ldA, ldB, ldC;
 
+	// entrada de dados para o lado 1
     printf("Digite o lado 1\n");
+    // atribuicao do valor digitado do lado 1 na variavel ldA
     scanf("%f", &ldA);
+    
+    // entrada de dados para o lado 2
     printf("Digite o lado 2\n");
+    // atribuicao do valor digitado do lado 1 na variavel ldB
     scanf("%f", &ldB);
+    
+    // entrada de dados para o lado 3
     printf("Digite o lado 3\n");
+    // atribuicao do valor digitado do lado 1 na variavel ldC
     scanf("%f", &ldC);
 
+	/*
+	*	verificacao das condicores da existencia de um triangulo onde temos essas condições 
+	*
+	*	1º if = | b - c | < a < b + c
+	*	2º if = | a - c | < b < a + c
+	*	3º if = | a - b | < c < a + b	
+	*
+	*/
+	
     if((ldB-ldC) < ldA  && ldA <(ldB+ldC))
     {
         if((ldA-ldC) < ldB  && ldB <(ldA+ldC))
         {
             if((ldA-ldB) < ldC  && ldC <(ldA+ldB))
             {
+            	
+            	/*
+            	Definicao dos triangulos onde:
+            	
+            	Triângulo escaleno: Todos os lados e ângulos são diferentes.
+            	
+            	Triângulos isósceles: dois lados iguais e os ângulos opostos a esses lados iguais. 
+            	
+            	Triângulo equilátero: Todos os lados e ângulos iguais. Concluímos que seus ângulos serão de 60°.
+            	*/
+            	
+            	
                 if(ldA==ldB && ldB==ldC){
                     printf("\nTriangulo equilatero.\n");
                 }else if(ldA==ldB || ldB==ldC || ldA==ldC){
